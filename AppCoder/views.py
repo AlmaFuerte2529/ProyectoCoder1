cb9dc2d610ec3f097abcd91c06a0ca2afa2050ee
Pablo Ariel Fiorini <106124388+AlmaFuerte2529@users.noreply.github.com>
2022-07-10 02:30:14 -0300

    segundo proyecto1

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,7 +1,9 @@
 from django.http.request import QueryDict
+from django import http
 from django.shortcuts import render, HttpResponse
-from AppCoder.models import Curso
+from AppCoder.models import Curso, Profesor
 from django.http import HttpResponse
+from AppCoder.forms import CursoForm, ProfeForm
 
 # Create your views here.
 
@@ -33,13 +35,65 @@ def entregables(request):
 def cursoFormulario(request):
 
     if (request.method=="POST"):
+        form= CursoForm(request.POST)
+        if form.is_valid():
+            info= form.cleaned_data
+            nombre= info["nombre"]
+            comision= info["comision"]
+            curso= Curso(nombre=nombre, comision=comision)
+            curso.save()
+            return render (request, "Appcoder/inicio.html")
+        
+    else:
+        form= CursoForm()
+
+    return render(request, "Appcoder/cursoFormulario.html", {"formulario":form})
+
+def profeFormulario(request):
+
+    if request.method=="POST":
+        form= ProfeForm(request.POST)
+        if form.is_valid():
+            info= form.cleaned_data
+            nombre= info["nombre"]
+            apellido= info["apellido"]
+            email= info["email"]
+            profesion= info["profesion"]
+            profe= Profesor(nombre=nombre, apellido=apellido, email=email, profesion=profesion)
+            profe.save()
+            return render (request, "Appcoder/inicio.html")
+    else:
+        form= ProfeForm()
+        return render(request, "Appcoder/profeform.html", {"formulario":form})
+        
+def busquedaComision(request):
+
+    return render(request, "Appcoder/busquedaComision.html")
+
+def buscar(request):
+    if request.GET["comision"]:
+        comi= request.GET["comision"]
+        cursos= Curso.objects.filter(comision__icontains=comi)
+        return render(request, "Appcoder/resultadosBusqueda.html", {"cursos":cursos})
+    else:
+        return render(request, "Appcoder/busquedaComision.html", {"error":"No se ingreso ninguna comision"})
+
+
+
+    #respuesta= f"estoy buscando la comision : {comision}"
+    #return HttpResponse(respuesta)
+
+
+"""
+def cursoFormulario(request):
+
+    if (request.method=="POST"):
 
         nombre= request.POST.get("curso")
         comision= request.POST.get("comision")
         curso = Curso(nombre=nombre, comision=comision)
         curso.save()
-
-        return render(request, "Appcoder/inicio.html")
+        return render (request, "Appcoder/inicio.html")
 
     return render(request, "Appcoder/cursoFormulario.html")
-
+    """
