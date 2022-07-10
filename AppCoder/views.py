04052834049eb5ee947bd57d300fa4b90756074f
Pablo Ariel Fiorini <106124388+AlmaFuerte2529@users.noreply.github.com>
2022-07-09 22:32:27 -0300

    segundo proyecto1

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http.request import QueryDict
+from django.shortcuts import render, HttpResponse
 from AppCoder.models import Curso
 from django.http import HttpResponse
 
